933911be7d737be336de4b9be13b8a2c39975e97
heeseok.noh <nhs0912@nate.com>
2022-12-13 21:39:44 +0900

    feat: init

---
+++ src/main/java/com/justone/kr/intercession/IntercessionApplication.java
@@ -0,0 +1,13 @@
+package com.justone.kr.intercession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justone/kr/intercession/IntercessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justone.kr.intercession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
